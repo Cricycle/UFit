7d709b9295a87796167aee37d1f08050ecc5f604
Jacob Meziani <jmeziani@ufl.edu>
2012-03-31 14:52:19 -0400

    profilecreationactivity updated

--- src/UFit/namespace/ProfileCreationActivity.java
+++ src/UFit/namespace/ProfileCreationActivity.java
@@ -5,24 +5,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 
 public class ProfileCreationActivity extends Activity implements OnClickListener {
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-// TODO Auto-generated method stub
-super.onCreate(savedInstanceState);
-setContentView(R.layout.profilecreation);
-View v = findViewById(R.id.profile_button_next);
-v.setOnClickListener(this);
-}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profilecreation);
+		Spinner spinner = (Spinner) findViewById(R.id.profile_spinner_gender);
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	            this, R.array.MF, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner.setAdapter(adapter);
+		
 
-public void onClick(View arg0) {
-if(arg0.getId() == R.id.profile_button_next){
-Intent intent = new Intent(this,GoalSelection.class);
-this.startActivity(intent);
-}
+		
+		View v = findViewById(R.id.profile_button_next);
+		TextView v2 = (TextView) findViewById(R.id.bmi2);
+		v.setOnClickListener(this);
+		v2.setText("23");
+	}
+	
+	public void onClick(View arg0) {
+		if(arg0.getId() == R.id.profile_button_next){
+			Intent intent = new Intent(this,GoalSelection.class);
+			this.startActivity(intent);
+		}
 
 }
 }
