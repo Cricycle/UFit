180cbc7875dbaee7cbc88a1c7fb8d34ad8510e83
John Spuler <registered99@gmail.com>
2012-04-02 15:43:35 -0400

    added in weekly planner and modifyprofile layouts. i will integrate them into the code tonight, as well as the rest of them

--- gen/UFit/namespace/BuildConfig.java
+++ gen/UFit/namespace/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package UFit.namespace;
+package ufit.namespace;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
