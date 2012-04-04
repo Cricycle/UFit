de0edf4caf3ea0c7a62872dca78e8aa06b1c445e
John Spuler <registered99@gmail.com>
2012-04-04 01:59:00 -0400

    Fixed whitespace

--- src/ufit/profilecreation/ModifyOptionsScreenActivity.java
+++ src/ufit/profilecreation/ModifyOptionsScreenActivity.java
@@ -36,7 +36,7 @@ public class ModifyOptionsScreenActivity extends Activity implements OnClickList
 			Intent intent = new Intent(this,WeeklyPlannerActivity.class);
 			this.startActivity(intent);	        
 		} else if(v.getId() == R.id.mod_equip){
-			Intent intent = new Intent(this,WeeklyPlannerActivity.class);
+			Intent intent = new Intent(this,MachineSelection.class);
 			this.startActivity(intent);	        
 		} else if(v.getId() == R.id.button0){
 			Intent intent = new Intent(this,HomeScreen.class);
